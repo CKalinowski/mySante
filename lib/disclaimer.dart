7483ffffd7af80eadd09548e3e55b9f43f15195f
Chloé K <chloe.kalinowski@limayrac.fr>
2023-04-21 16:50:45 +0200

    page disclaimer clean

--- lib/disclaimer.dart
+++ lib/disclaimer.dart
@@ -2,8 +2,9 @@ import 'package:adobe_xd/page_link.dart';
 import 'package:adobe_xd/pinned.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import './btn_je_comprend.dart';
+import 'btn_je_comprends.dart';
 import './page1.dart';
 
 class Disclaimer extends StatelessWidget {
@@ -11,6 +12,89 @@ class Disclaimer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff16679a),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 250,
+                height: 250,
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    width: 350,
+                    height: 400,
+                    margin: EdgeInsets.only(top: 80),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  Container(
+                    width: 160,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(500),
+                    ),
+                  ),
+                  Container(
+                    width: 140,
+                    height: 140,
+                    margin: EdgeInsets.only(top: 10),
+                    decoration: BoxDecoration(
+                      color: Color(0xff07beb8),
+                      borderRadius: BorderRadius.circular(500),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.topCenter,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: FaIcon(
+                            FontAwesomeIcons.triangleExclamation,
+                            color: Colors.white,
+                            size: 80,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 200, left: 30, right: 30),
+                    child: Text(
+                      'Cette application a été crée à but informatif et ne remplace en aucun cas la consultation chez un professionnel de la santé. Merci de votre compréhension.',
+                      style: TextStyle(
+                        fontFamily: 'Segoe UI',
+                        fontSize: 20,
+                        color: const Color(0xff707070),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
+                    width: 289.0,
+                    height: 56.0,
+                    margin: EdgeInsets.only(top: 370),
+                    child: const BTNJeComprend(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
     return Scaffold(
       backgroundColor: const Color(0xffffffff),
       body: Stack(
@@ -51,7 +135,8 @@ class Disclaimer extends StatelessWidget {
               height: 189.0,
               decoration: const BoxDecoration(
                 color: Color(0xffffffff),
-                borderRadius: BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
               ),
             ),
           ),
@@ -62,7 +147,8 @@ class Disclaimer extends StatelessWidget {
               height: 150.0,
               decoration: const BoxDecoration(
                 color: Color(0xff07beb8),
-                borderRadius: BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
               ),
             ),
           ),
