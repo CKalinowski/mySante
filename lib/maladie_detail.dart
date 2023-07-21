b034d74a14acb8b0aee554455ce9756083bd3521
Ruben <ruben.vidot@mobiledev-pro.fr>
2023-07-21 15:20:22 +0200

    commit Chloé : traduction zoom maladie

--- lib/maladie_detail.dart
+++ lib/maladie_detail.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import 'btn_recommencer.dart';
 import 'doctolib.dart';
 import 'expendable_button.dart';
 import 'header.dart';
+import 'main.dart';
 import 'model/disease.dart';
+import 'model/langue_choose.dart';
 
 class MaladieDetail extends StatelessWidget {
   const MaladieDetail({Key? key, required this.disease}) : super(key: key);
   final Disease disease;
+  final String textRDV = 'N\'hésitez pas à prendre RDV chez un spécialiste sur ';
+  final String textPersonnesARisque = 'Personnes à risque';
+  final String textPersonnesARisqueNonDisponible = 'Personnes à risque non disponible';
+  final String textMedicaments = 'Médicaments';
+  final String textMedicamentsNonDisponible = 'Médicaments non disponible';
+  final String textDepistage = 'Dépistage';
+  final String textDepistageNonDisponible = 'Dépistage non disponible';
 
   @override
   Widget build(BuildContext context) {
     String proceduresText = '';
     if (disease.testsProceduresList != null) {
-      proceduresText = disease.testsProceduresList!
-          .map((testProcedure) =>
-              testProcedure.nomFr ?? 'Procédure non disponible')
-          .join('\n');
+      proceduresText = disease.testsProceduresList!.map((testProcedure) => testProcedure.nomFr ?? 'Procédure non disponible').join('\n');
     } else {
       proceduresText = 'Aucune procédure de test disponible.';
     }
@@ -59,28 +66,54 @@ class MaladieDetail extends StatelessWidget {
                       textAlign: TextAlign.center,
                     ),
                     const SizedBox(height: 20.0),
-                    ExpandableButton(
-                        buttonName: 'Dépistage', label: proceduresText),
+                    FutureBuilder(
+                        future: translator.translate('$textDepistage%$textDepistageNonDisponible',
+                            from: 'fr', to: context.watch<LangueChoose>().isEnglish ? 'en' : 'fr'),
+                        builder: (context, snapshot) {
+                          return ExpandableButton(
+                            buttonName: snapshot.hasData ? snapshot.data.toString().split('%')[0] : textDepistage,
+                            label: context.watch().isEnglish ? proceduresText : proceduresText,
+                          );
+                        }),
                     const SizedBox(height: 15.0),
-                    ExpandableButton(
-                        buttonName: 'Médicaments',
-                        label: disease.medicationDescription?.descriptionFr ??
-                            'Médicaments non trouvés'),
+                    FutureBuilder(
+                        future: translator.translate('$textMedicaments%$textMedicamentsNonDisponible',
+                            from: 'fr', to: context.watch<LangueChoose>().isEnglish ? 'en' : 'fr'),
+                        builder: (context, snapshot) {
+                          return ExpandableButton(
+                            buttonName: snapshot.hasData ? snapshot.data.toString().split('%')[0] : textMedicaments,
+                            label: context.watch().isEnglish
+                                ? disease.medicationDescription?.descriptionEn ??
+                                    (snapshot.hasData ? snapshot.data.toString().split('%')[1] : textMedicamentsNonDisponible)
+                                : disease.medicationDescription?.descriptionFr ?? textMedicamentsNonDisponible,
+                          );
+                        }),
                     const SizedBox(height: 15.0),
-                    ExpandableButton(
-                      buttonName: 'Personnes à risque',
-                      label: disease.whois?.descFr ??
-                          'Personnes à risque non disponible',
-                    ),
+                    FutureBuilder(
+                        future: translator.translate('$textPersonnesARisque%$textPersonnesARisqueNonDisponible',
+                            from: 'fr', to: context.watch<LangueChoose>().isEnglish ? 'en' : 'fr'),
+                        builder: (context, snapshot) {
+                          return ExpandableButton(
+                            buttonName: snapshot.hasData ? snapshot.data.toString().split('%')[0] : textPersonnesARisque,
+                            label: context.watch().isEnglish
+                                ? disease.whois?.descEn ??
+                                    (snapshot.hasData ? snapshot.data.toString().split('%')[1] : textPersonnesARisqueNonDisponible)
+                                : disease.whois?.descFr ?? textPersonnesARisqueNonDisponible,
+                          );
+                        }),
                     const SizedBox(height: 15.0),
-                    Text(
-                      'N\'hésitez pas à prendre RDV chez un spécialiste sur ',
-                      style: GoogleFonts.lato(
-                        fontSize: 18,
-                        color: const Color(0xff707070),
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
+                    FutureBuilder(
+                        future: translator.translate(textRDV, from: 'fr', to: context.watch<LangueChoose>().isEnglish ? 'en' : 'fr'),
+                        builder: (context, snapshot) {
+                          return Text(
+                            snapshot.hasData ? snapshot.data.toString() : textRDV,
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              color: const Color(0xff707070),
+                            ),
+                            textAlign: TextAlign.center,
+                          );
+                        }),
                     const Doctolib(),
                     const SizedBox(height: 20.0),
                     Container(
